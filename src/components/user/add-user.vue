f5e550042507b20772cc6d3ce74ae7ee4687fa05
Muhammad Ovais Siddiqui <ovais.siddiqui@mtfxgroup.com>
2025-12-01 14:34:59 +0500

    added shadcn UI library

---
+++ src/components/user/add-user.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50),
+}))
+
+const form = useForm({
+  validationSchema: formSchema,
+})
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log('Form submitted!', values)
+})
+</script>
+
+<template>
+  <form @submit="onSubmit">
+    ...
+  </form>
+</template>
